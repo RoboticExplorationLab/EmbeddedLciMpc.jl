b46bcf7330227d2e4bf9a52600cb095f388fbb2c
ChiyenLee <chiyenl@cs.cmu.edu>
2022-04-27 14:36:55 -0400

    integrate juliacpp with A1_Ctrl

--- README.md
+++ README.md
@@ -22,3 +22,18 @@ You can see the current example with
 ```
 ./main
 ```
+
+## Issues and TODOs
+1. ContactImplicitMPC uses meshcat, which can't be built in the docker environment without some dependencies
+    * TODO: add this to Dockerfile `apt get install -y unzip`
+2. Need to install Julia on docker environment
+    * TODO: add the following
+    ```
+    curl https://julialang-s3.julialang.org/bin/linux/x64/1.6/julia-1.6.6-linux-x86_64.tar.gz -o julia-1.6.6-linux-x86_64.tar.gz
+
+    tar -xvzf julia-1.6.6-linux-x86_64.tar.gz
+
+    sudo mv julia-1.6.6/ /opt/
+
+    sudo ln -s /opt/julia-1.6.6/bin/julia /usr/local/bin/julia
+    ```
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,28 @@ JULIA_DEFINE_FAST_TLS()
 
 using namespace std; 
 int main() {
+    jl_init();
+    jl_eval_string("using Pkg");
+    jl_eval_string("Pkg.activate(\"../\")"); 
+    jl_eval_string("using EmbeddedLciMpc");
 
+    // Important: We have to get a reference to the module in order to get the function
+    jl_module_t* EmbeddedLciMpc = (jl_module_t *)jl_eval_string("EmbeddedLciMpc");
+
+    // Example of calling a function with a 1D array 
+    jl_function_t *times2 = jl_get_function(EmbeddedLciMpc, "times2array"); 
+    Eigen::MatrixXd test(10, 10); test.setIdentity(); 
+    JuliaVector2d jv(test); 
+    JuliaVector2d result((jl_array_t*) jl_call1(times2, (jl_value_t*) jv.toJArray())); 
+    cout << result.toEigenMatrix() << endl; 
+
+    
+    // Example of calling a function that returns a 1D array 
+    jl_function_t *findEigs = jl_get_function(EmbeddedLciMpc, "findEigs"); 
+    jl_array_t *j_result = (jl_array_t*) jl_call1(findEigs, (jl_value_t*) jv.toJArray());
+    JuliaVector2d result2(j_result); 
+    cout << result2.toEigenMatrix() << endl; 
+
+    jl_atexit_hook(0);
     return -1; 
 }
