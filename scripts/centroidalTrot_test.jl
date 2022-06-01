2d63f7ea80a1f8c47571eb10d653d86adfaadc6e
Chiyen Lee <chiyenn.lee@gmail.com>
2022-05-31 20:37:40 -0400

    sideway velocity tracking configuration

--- scripts/centroidalTrot_test.jl
+++ scripts/centroidalTrot_test.jl
@@ -179,12 +179,14 @@ h_sim = h / N_sample
 H_sim = 320
 κ_mpc = 2.0e-4
 
-v0 = 0.2
+v0 = 0.0
 obj = TrackingVelocityObjective(model, env, H_mpc,
-v = h/H_mpc * [Diagonal([[1,1,15]; [6000,6000,8000]; 2e-3 * fill([1,1,1], 4)...]) for t = 1:H_mpc],
+v = h/H_mpc * [Diagonal([[1,0.5,15]; [6000,6000,8000]; 2e-3 * fill([1,1,1], 4)...]) for t = 1:H_mpc],
 q = h/H_mpc * [LciMPC.relative_state_cost([0,0,1000], [1200,1200,1200], [4,4,20]) for t = 1:H_mpc],
 u = h/H_mpc * [Diagonal(9e-3 * vcat(fill([1,1,1], 4)...)) for t = 1:H_mpc],
 v_target = [1/ref_traj.h * [v0;0;0; 0;0;0; v0;0;0; v0;0;0; v0;0;0; v0;0;0] for t = 1:H_mpc],)
+# v_target = [1/ref_traj.h * [0;v0;0; 0;0;0; 0;v0;0; 0;v0;0; 0;v0;0; 0;v0;0] for t = 1:H_mpc],)
+
 
 # velocity tracking 
 # v0 = 0.2
