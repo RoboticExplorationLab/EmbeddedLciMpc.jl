f8798276ae845ea13286d43ad1e73866d2996cbe
Chiyen Lee <chiyenn.lee@gmail.com>
2022-06-02 11:11:02 -0400

    troting param tune

--- scripts/centroidalTrot_test.jl
+++ scripts/centroidalTrot_test.jl
@@ -264,8 +264,8 @@ H_sim = 320
 
 v0 = 0.0
 obj = TrackingVelocityObjective(model, env, H_mpc,
-v = h/H_mpc * [Diagonal([[10,20,15]; [6000,6000,800]; 2e-3 * fill([2,2,1], 4)...]) for t = 1:H_mpc],
-q = h/H_mpc * [LciMPC.relative_state_cost([0,0,1000], [1200,1200,1200], [5,5,15]) for t = 1:H_mpc],
+v = h/H_mpc * [Diagonal([[10,20,15]; [6000,6000,800]; 2e-3 * fill([0.7,0.7,1], 4)...]) for t = 1:H_mpc],
+q = h/H_mpc * [LciMPC.relative_state_cost([0,0,1000], [1200,1200,600], [5,5,15]) for t = 1:H_mpc],
 u = h/H_mpc * [Diagonal(9e-3 * vcat(fill([1,1,1], 4)...)) for t = 1:H_mpc],
 v_target = [1/ref_traj.h * [v0;0;0; 0;0;0; v0;0;0; v0;0;0; v0;0;0; v0;0;0] for t = 1:H_mpc],)
 # v_target = [1/ref_traj.h * [0;v0;0; 0;0;0; 0;v0;0; 0;v0;0; 0;v0;0; 0;v0;0] for t = 1:H_mpc],)
