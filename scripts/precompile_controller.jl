fd56c7f68cd1d838bc1310016db0a9102059ac77
johnzhang3 <johnzhang@cmu.edu>
2022-08-26 12:18:09 -0400

    new ref

--- scripts/precompile_controller.jl
+++ scripts/precompile_controller.jl
@@ -1,5 +1,8 @@
 
-include("centroidalStand_test.jl")
+
+include("imitation_mocap/pace_forward_test.jl")
+println("here!")
+# include("centroidalStand_test.jl")
 # include("centroidalTrot_test.jl")
 # include("centroidalWall_test.jl")
 # include("centroidalOneLeg_test.jl")
