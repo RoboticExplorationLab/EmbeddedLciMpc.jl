1f4637309bdee32eb3f5042c0aa665c9653a517c
johnzhang3 <johnzhang@cmu.edu>
2022-09-23 12:04:42 -0400

    new reach traj

--- scripts/reach_policy.jl
+++ scripts/reach_policy.jl
@@ -17,8 +17,8 @@ env = s.env
 
 # ## Reference Trajectory Generation 
 ref_traj = deepcopy(get_trajectory(s.model, s.env,
-joinpath(CIMPC_path, "../examples/A1-imitation/results/1005/run34/1005_tol0.001.jld2"), 
-# joinpath(CIMPC_path, "../examples/A1-imitation/results/1005/run37/1005_tol0.001.jld2"), 
+# joinpath(CIMPC_path, "../examples/A1-imitation/results/1005/run1/1005_tol0.001.jld2"), 
+joinpath(CIMPC_path, "../examples/A1-imitation/results/1005/run37/1005_tol0.001.jld2"), 
 
 load_type = :split_traj_alt));
 
