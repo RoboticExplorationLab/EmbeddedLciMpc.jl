c3dc0380d7a2e2f6a019fdfed20f729bafaf3f79
John <johnzhang@cmu.edu>
2022-10-17 13:06:53 -0400

    important bug fix for importing cost weights

--- scripts/stand_policy.jl
+++ scripts/stand_policy.jl
@@ -47,11 +47,11 @@ max_iter_nt = Int(config["max_iter_nt"])
 # tracking objective 
 # standing velocity tracking 
 v_weights = Diagonal([[config["w_v_pos_x"], config["w_v_pos_y"], config["w_v_pos_z"]]; 
-                    [config["w_v_ang_z"], config["w_v_ang_z"], config["w_v_ang_z"]]; 
+                    [config["w_v_ang_z"], config["w_v_ang_y"], config["w_v_ang_x"]]; 
                     fill([config["w_v_ft_x"], config["w_v_ft_y"], config["w_v_ft_z"]], 4)...])
 
 q_weights = Diagonal([[config["w_q_pos_x"], config["w_q_pos_y"], config["w_q_pos_z"]]; 
-                    [config["w_q_ang_z"], config["w_q_ang_z"], config["w_q_ang_z"]]; 
+                    [config["w_q_ang_z"], config["w_q_ang_y"], config["w_q_ang_x"]]; 
                     fill([config["w_q_ft_x"], config["w_q_ft_y"], config["w_q_ft_z"]], 4)...])
                      
 u_weights = Diagonal(vcat(fill([config["w_u_1"], config["w_u_2"], config["w_u_3"]], 4)...))
