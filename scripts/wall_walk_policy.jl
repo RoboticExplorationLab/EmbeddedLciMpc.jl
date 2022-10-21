14c7b64a3a858bae7382001d3edbe341c59ddfb2
Arun Bishop <arunleob@umich.edu>
2022-10-21 17:24:56 -0400

    working on wall walk hardware

--- scripts/wall_walk_policy.jl
+++ scripts/wall_walk_policy.jl
@@ -17,7 +17,7 @@ env = s.env
 
 # ## Reference Trajectory Generation 
 ref_traj = deepcopy(get_trajectory(s.model, s.env,
-	joinpath(CIMPC_path, "../examples/centroidal_quadruped_wall/reference/stand_wall_two_steps_combined_v7.jld2"),
+	joinpath(CIMPC_path, "../examples/centroidal_quadruped_wall/reference/stand_wall_two_steps_combined_v8.jld2"),
     load_type = :split_traj_alt))
 
 
