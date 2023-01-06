cf1288748bf766a66bebed7b6a58233150859e1f
Arun Bishop <arunleob@cmu.edu>
2023-01-06 12:21:11 -0500

    reach demo

--- scripts/reach_policy.jl
+++ scripts/reach_policy.jl
@@ -17,7 +17,7 @@ env = s.env
 
 # ## Reference Trajectory Generation 
 ref_traj = deepcopy(get_trajectory(s.model, s.env,
-joinpath(CIMPC_path, "../examples/A1-imitation/results/1005/run11/1005.jld2"), 
+joinpath(CIMPC_path, "../examples/A1-imitation/results/1005/run12/1005.jld2"), 
 # joinpath(CIMPC_path, "../examples/A1-imitation/results/1005/run37/1005_tol0.001.jld2"), 
 
 load_type = :split_traj_alt));
